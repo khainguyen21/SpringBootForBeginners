721b1db178c63f164c77b9e42f535cb34059c972
Khai Nguyen <khainguyen2004@gmail.com>
2025-07-22 15:33:31 -0700

    Practice Springboot

--- src/main/java/com/khaidevcode/SpringBootForBeginnersApplication.java
+++ src/main/java/com/khaidevcode/SpringBootForBeginnersApplication.java
@@ -2,12 +2,19 @@ package com.khaidevcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootForBeginnersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootForBeginnersApplication.class, args);
     }
 
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World SpringBoot";
+    }
 }
