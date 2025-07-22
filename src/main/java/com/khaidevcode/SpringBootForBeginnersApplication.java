218254c76ad1c241fef75620828b9c3cbbe85ffc
Khai Nguyen <khainguyen2004@gmail.com>
2025-07-22 14:57:12 -0700

    First commit on Springboot

---
+++ src/main/java/com/khaidevcode/SpringBootForBeginnersApplication.java
@@ -0,0 +1,13 @@
+package com.khaidevcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForBeginnersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootForBeginnersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khaidevcode/SpringBootForBeginnersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khaidevcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootForBeginnersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
