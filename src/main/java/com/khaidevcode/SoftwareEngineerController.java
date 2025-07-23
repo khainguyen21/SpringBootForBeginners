74fd07e22cbd9dd645037348f62790f99c20e534
Khai Nguyen <khainguyen2004@gmail.com>
2025-07-22 17:18:36 -0700

    Add basic SoftwareEngineer API and PostgreSQL setup

---
+++ src/main/java/com/khaidevcode/SoftwareEngineerController.java
@@ -0,0 +1,23 @@
+package com.khaidevcode;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers () {
+        return List.of(
+                new SoftwareEngineer(1, "Alex", Collections.singletonList("js, node, react, tailwindcss")),
+                new SoftwareEngineer(2, "Jamila", Collections.singletonList("java, spring, springboot"))
+
+        );
+    }
+}
